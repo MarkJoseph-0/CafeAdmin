ad2267f3778b7c7dab6be664b671f717bcffdb07
MarkJoseph-0 <markjoseph.lopena741@gmail.com>
2025-02-27 14:33:26 +0800

    Add files via upload

---
+++ CafeAdmin/src/main/java/com/mycompany/cafeadmin/CafeAdmin.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cafeadmin;
+
+/**
+ *
+ * @author markl
+ */
+public class CafeAdmin {
+
+    public static void main(String[] args) {
+        Login lg = new Login();
+        lg.setVisible(true);
+    }
+}
